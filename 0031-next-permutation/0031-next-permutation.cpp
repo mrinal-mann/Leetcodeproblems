5bb7bcee0b84e80fe6e64b4456f9a36f393c00be
Mrinal Manna <118587617+mrinal-mann@users.noreply.github.com>
2024-09-16 22:01:39 +0530

    Time: 0 ms (100.00%), Space: 14.8 MB (83.29%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int index=-1;
+        int index=-1; // figure the break point a[i]<a[i+1]
         int n=nums.size();
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
@@ -9,17 +9,17 @@ public:
                 break;
             }
         }
-        if(index==-1){
+        if(index==-1){  //edge case is the index is -1
             reverse(nums.begin(),nums.end());
             return;
         }
-        for(int i=n-1;i>index;i--){
+        for(int i=n-1;i>index;i--){  // swap the breakpoint element with the smallest element close to the breakpoint
             if(nums[i]>nums[index]){
                 swap(nums[i],nums[index]);
                 break;
             }
         }
-        reverse(nums.begin()+index+1,nums.end());
+        reverse(nums.begin()+index+1,nums.end()); //reverse the remaining elements
         return;
         
     }
