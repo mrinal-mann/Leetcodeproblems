68c1946d3fef1ba2582c5bb1a2e3ba4aff44056f
Mrinal Manna <118587617+mrinal-mann@users.noreply.github.com>
2024-10-24 20:20:18 +0530

    Time: 7 ms (63.74%), Space: 12.8 MB (65.10%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,10 +9,16 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        node->val=node->next->val;
-        ListNode *nodetodelete=node->next;
-        node->next=node->next->next;
-        delete nodetodelete;
-        
+        // Step 1: Copy the value of the next node to the current node
+        node->val = node->next->val;
+
+        // Step 2: Store the node to be deleted (which is the next node)
+        ListNode* nodeToDelete = node->next;
+
+        // Step 3: Bypass the next node
+        node->next = node->next->next;
+
+        // Step 4: Delete the bypassed node
+        delete nodeToDelete;
     }
 };
